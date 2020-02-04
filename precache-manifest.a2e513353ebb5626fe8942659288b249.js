3f1cb1ac24c6d63647706f135ffb300cc35de826
Shayne Leitman <sleitman17@gmail.com>
2020-02-04 13:48:38 -0500

    Updates

---
+++ precache-manifest.a2e513353ebb5626fe8942659288b249.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acdafbfc670edb385ecfc2dd81bbb77e",
+    "url": "/my-quiz/index.html"
+  },
+  {
+    "revision": "c9c4f81635f4e60fe310",
+    "url": "/my-quiz/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "146ad042750fda6a7114",
+    "url": "/my-quiz/static/css/main.6b611026.chunk.css"
+  },
+  {
+    "revision": "c9c4f81635f4e60fe310",
+    "url": "/my-quiz/static/js/2.b063e8f1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-quiz/static/js/2.b063e8f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "146ad042750fda6a7114",
+    "url": "/my-quiz/static/js/main.b1d9b7ff.chunk.js"
+  },
+  {
+    "revision": "8c9c15e8abea6e451ed8",
+    "url": "/my-quiz/static/js/runtime-main.b8cd6803.js"
+  }
+]);
